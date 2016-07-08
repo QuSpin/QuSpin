7d14e65ba2178327d52116e0203784b0fe8ae7f5
mbukov <mbukov@bu.edu>
2016-07-08 16:59:57 -0400

    added test_photon and things to to-do list.

--- test_photon.py
+++ test_photon.py
@@ -8,8 +8,9 @@ from numpy.random import random,seed
 
 seed()
 
+n_jobs=2
 
-L = 8
+L = 14
 Ntot = L
 
 
@@ -50,21 +51,46 @@ basis_sp = spin_basis_1d(L=L,kblock=0,pblock=1)
 H_sp_ph=hamiltonian(static_sp_ph,dynamic_sp_ph,L=L,dtype=np.float64,basis=basis_sp_ph)
 H_sp=hamiltonian(static_sp,dynamic_sp_ph,L=L,dtype=np.float64,basis=basis_sp)
 
+
+Ns = H_sp.Ns
+
 print "spin-photon H-space size is {}".format(H_sp_ph.Ns)
-print "spin H-space size is {}".format(H_sp.Ns)
+print "spin H-space size is {}".format(Ns)
 
 E_sp_ph = H_sp_ph.eigvalsh()
 E_sp = H_sp.eigvalsh()
 
+# this function evolves the ith local basis state with Hamiltonian H
+# this is used to construct the stroboscpoic evolution operator
+def evolve(i,H,T):
+	from numpy import zeros
+	from scipy.integrate import complex_ode
+
+	nsteps=sum([2**_i for _i in xrange(32,63)]) # huge number to make sure solver is successful.
+	psi0=zeros((Ns,),dtype=np.complex128); psi0[i]=1.0
+	solver=complex_ode(H._hamiltonian__SO)
+	solver.set_integrator('dop853', atol=10**-15,rtol=10**-15,nsteps=nsteps) 
+	solver.set_initial_value(psi0,t=0.0)
+	solver.integrate(T)
+	if solver.successful():
+		return solver.y
+	else:
+		raise Exception('failed to integrate')
+
+
+### USING JOBLIB ###
+def get_U(H,n_jobs,T): 
+	from joblib import delayed,Parallel
+	from numpy import vstack # or hstack, I can't remember
+
+	sols=Parallel(n_jobs=n_jobs)(delayed(evolve)(i,H,T) for i in xrange(Ns))
+
+	return vstack(sols)
+
 
 # calculate Floquet Hamiltonian
-identity = np.ones(H_sp._shape, dtype=np.float64)
-U_F = np.zeros(H_sp._shape, dtype=np.complex128)
-for i in xrange(H_sp.Ns):
-	solver = sp.integrate.complex_ode(H_sp._hamiltonian__SO)
-	print solver
-	U_F[:,i] = 1
-
+U_F = get_U(H_sp,n_jobs,T)
+#print evolve(0,H_sp,T)
 
 
 
