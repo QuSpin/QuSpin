e744cdbe41bb84425d70241921164d529ff3e500
Phillip Weinberg <weinbe58@bu.edu>
2017-02-11 13:55:49 -0500

    removing many extra files

--- quspin/basis/basis_1d/_constructors/sources/basis/n_basis.pyx
+++
@@ -1,8 +0,0 @@
-
-cdef void make_n_basis_template(ns_type next_state,void *ns_pars,state_type MAX,state_type s,basis_type*basis):
-	cdef state_type i
-	for i in range(MAX):
-		basis[i] = s
-		s = next_state(s,ns_pars)	
-
-
--- quspin/basis/basis_1d/_constructors/sources/op/op.pyx
+++
@@ -1,9 +0,0 @@
-
-cdef int op_template(op_type op_func, void *op_pars, index_type Ns, basis_type *basis,
-					 str opstr, NP_INT32_t *indx, scalar_type J, index_type *row, index_type *col, matrix_type *ME):
-	cdef index_type i
-
-	for i in range(Ns):
-		col[i] = i
-
-	return op_func(Ns,basis,opstr,indx,J,row,ME,op_pars)
