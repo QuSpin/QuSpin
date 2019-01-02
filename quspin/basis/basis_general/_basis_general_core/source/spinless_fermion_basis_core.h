a01bf7d3508975c0f4c05fd2235d88eb48d3f1da
weinbe58 <weinbe58@gmail.com>
2019-01-01 23:54:10 -0500

    simplifying spinful_fermion_general basis.

--- quspin/basis/basis_general/_basis_general_core/source/spinless_fermion_basis_core.h
+++ quspin/basis/basis_general/_basis_general_core/source/spinless_fermion_basis_core.h
@@ -60,36 +60,11 @@ void getf_count(int nums[], int left, int right, bool &f_count){
 
 
 
-// template<class I>
-// I inline spinless_fermion_map_bits(I s,const int map[],const int N,int &sign){
-// 	I ss = 0;
-// 	int pos_list[64];
-// 	int np = 0;
-// 	I sf_count = ((inv&s) & (bit_info<I>::eob >> (N&1)));
-// 	bool f_count = (bit_count(sf_count,0)&1);
-
-// 	s ^= inv;
-
-// 	for(int i=N-1;i>=0;--i){
-// 		if(s&1){
-// 			pos_list[np]=j;++np;
-// 			ss ^= ( I(1)<<(N-map[i]-1) );
-// 		}
-// 		s >>= 1;
-// 	}
-
-// 	if(np>1){getf_count(pos_list,0,np-1,f_count);}
-// 	if(f_count){sign ^= (-2);}
-
-// 	return ss;
-// }
-
-
 
 template<class I>
 I inline spinless_fermion_map_bits(I s,const int map[],const int N,int &sign){
 	I ss = 0;
-	int pos_list[64];
+	int pos_list[bit_info<I>::bits];
 	int np = 0;
 	bool f_count = 0;
 
@@ -118,13 +93,14 @@ class spinless_fermion_basis_core : public hcb_basis_core<I>
 {
 	public:
 		spinless_fermion_basis_core(const int _N) : \
-		hcb_basis_core<I>::hcb_basis_core(_N) {}
+		hcb_basis_core<I>::hcb_basis_core(_N) { }
 
 		spinless_fermion_basis_core(const int _N,const int _nt,const int _maps[], \
 						   const int _pers[], const int _qs[]) : \
 		hcb_basis_core<I>::hcb_basis_core(_N,_nt,_maps,_pers,_qs) {}
 
-		~spinless_fermion_basis_core() {}
+		~spinless_fermion_basis_core(){}
+
 
 		I map_state(I s,int n_map,int &sign){
 			if(general_basis_core<I>::nt<=0){
@@ -148,6 +124,29 @@ class spinless_fermion_basis_core : public hcb_basis_core<I>
 				sign[i] = temp_sign;
 			}
 		}
+
+		// I map_state(I s,int n_map,int &sign){
+		// 	if(general_basis_core<I>::nt<=0){
+		// 		return s;
+		// 	}
+		// 	const int n = general_basis_core<I>::N;
+		// 	return spinless_fermion_map_bits(s,&general_basis_core<I>::maps[n_map*n],n,sign);
+			
+		// }
+
+		// void map_state(I s[],npy_intp M,int n_map,signed char sign[]){
+		// 	if(general_basis_core<I>::nt<=0){
+		// 		return;
+		// 	}
+		// 	const int n = general_basis_core<I>::N;
+		// 	const int * map = &general_basis_core<I>::maps[n_map*n];
+		// 	#pragma omp for schedule(static,1)
+		// 	for(npy_intp i=0;i<M;i++){
+		// 		int temp_sign = sign[i];
+		// 		s[i] = spinless_fermion_map_bits(s[i],map,n,temp_sign);
+		// 		sign[i] = temp_sign;
+		// 	}
+		// }
 
 		int op(I &r,std::complex<double> &m,const int n_op,const char opstr[],const int indx[]){
 			I s = r;
