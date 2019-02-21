48a8a668d8067409859f04358da54b6a36bb32a3
weinbe58 <weinbe58@bu.edu>
2019-02-20 20:19:07 -0500

    tests passed pyhon 36 on osx

--- quspin/basis/setup.py
+++ quspin/basis/setup.py
@@ -1,22 +1,22 @@
 
 def get_include_dirs():
-    from sysconfig import get_paths
-    import numpy,os,sys
+	from sysconfig import get_paths
+	import numpy,os,sys
 
-    package_dir = os.path.dirname(os.path.realpath(__file__))
-    data_path = get_paths()["data"]
+	package_dir = os.path.dirname(os.path.realpath(__file__))
+	data_path = get_paths()["data"]
 
-    package_dir = os.path.expandvars(package_dir)
-    data_path = os.path.expandvars(data_path)
+	package_dir = os.path.expandvars(package_dir)
+	data_path = os.path.expandvars(data_path)
 
-    include_dirs = [numpy.get_include()]
-    include_dirs.append(os.path.join(package_dir,"source"))
-    if sys.platform == "win32":
-        include_dirs.append(os.path.join(data_path,"Library","include"))
-    else:
-        include_dirs.append(os.path.join(data_path,"include"))
+	include_dirs = [numpy.get_include()]
+	include_dirs.append(os.path.join(package_dir,"source"))
+	if sys.platform == "win32":
+		include_dirs.append(os.path.join(data_path,"Library","include"))
+	else:
+		include_dirs.append(os.path.join(data_path,"include"))
 
-    return include_dirs
+	return include_dirs
 
 
 def cython_files():
@@ -35,7 +35,7 @@ def cython_files():
 
 def configuration(parent_package='',top_path=None):
 	from numpy.distutils.misc_util import Configuration
-	import os,numpy
+	import os,numpy,sys
 	config = Configuration('basis', parent_package, top_path)
 	config.add_subpackage('basis_1d')
 	config.add_subpackage('basis_general')
@@ -45,10 +45,15 @@ def configuration(parent_package='',top_path=None):
 
 	package_dir = os.path.dirname(os.path.realpath(__file__))
 	package_dir = os.path.expandvars(package_dir)
+	extra_compile_args=[]
+	extra_link_args=[]  
+	  
+	if sys.platform == "darwin":
+		extra_compile_args = ["-std=c++11"]
 
 	basis_utils_src = os.path.join(package_dir,"_basis_utils.cpp")	
 	config.add_extension('_basis_utils',sources=basis_utils_src,include_dirs=get_include_dirs(),
-							language="c++")
+							language="c++",extra_compile_args=extra_compile_args,extra_link_args=extra_link_args)
 
 	return config
 
