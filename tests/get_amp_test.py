fixing various issues; precision issue with get_amp remains

tests/get_amp_test.py
@@ -6,14 +6,13 @@ sys.path.insert(0,quspin_path)
 
 from quspin.operators import hamiltonian
 from quspin.basis import spin_basis_general, boson_basis_general, spinless_fermion_basis_general, spinful_fermion_basis_general
+from quspin.basis.transformations import square_lattice_trans
 import numpy as np
 
 
 
 #
 ###### define model parameters ######
-J1=1.0 # spin=spin interaction
-J2=0.5 # magnetic field strength
 Lx, Ly = 4, 4 # linear dimension of 2d lattice
 N_2d = Lx*Ly # number of sites
 #
@@ -24,31 +23,22 @@ y = s//Lx # y positions for sites
 
 T_x = (x+1)%Lx + Lx*y # translation along x-direction
 T_y = x +Lx*((y+1)%Ly) # translation along y-direction
-
-#R = np.rot90(s.reshape(Lx,Ly), axes=(0,1)).reshape(N_2d) # rotate
-P_d = y + Lx*x
-
-P_x = x + Lx*(Ly-y-1) # reflection about x-axis
-P_y = (Lx-x-1) + Lx*y # reflection about y-axis
-
 Z   = -(s+1) # spin inversion
 
 
-J_p=[[np.sqrt(7),i,T_x[i]] for i in range(N_2d)] + [[np.sqrt(7),i,T_y[i]] for i in range(N_2d)]
+J_p=[[+np.sqrt(7),i,T_x[i]] for i in range(N_2d)] + [[+np.sqrt(7),i,T_y[i]] for i in range(N_2d)]
 J_n=[[-np.sqrt(7),i,T_x[i]] for i in range(N_2d)] + [[-np.sqrt(7),i,T_y[i]] for i in range(N_2d)]
 
+lattice_trans=square_lattice_trans(Lx,Ly)
+allowed_sectors=lattice_trans.allowed_blocks_iter()
 
-for q in [0,Lx-1]: # loop only over quantum numbers 0, pi so all symmetries commute
+for ii,basis_dict in enumerate(allowed_sectors):
 
 	###### setting up bases ######
 
 	basis_boson = boson_basis_general(N_2d, make_basis=False,
 										Nb=N_2d//4,sps=2,
-										kxblock=(T_x,q),kyblock=(T_y,q),
-										rblock=(P_d,0),
-										pxblock=(P_x,0),pyblock=(P_y,0),
-										block_order=['pyblock','pxblock','rblock','kyblock','kxblock']
-									#	block_order=['kyblock','kxblock','pyblock','pxblock','rblock',]
+										**basis_dict,
 									)
 
 	basis_boson_full = boson_basis_general(N_2d, make_basis=False,
@@ -58,9 +48,7 @@ for q in [0,Lx-1]: # loop only over quantum numbers 0, pi so all symmetries comm
 
 	basis_spin = spin_basis_general(N_2d, pauli=False, make_basis=False,
 										Nup=N_2d//2,
-										kxblock=(T_x,q),kyblock=(T_y,q),
-										rblock=(P_d,0),
-										pxblock=(P_x,0),pyblock=(P_y,0),
+										**basis_dict,
 										zblock=(Z,0)
 									)
 
@@ -70,9 +58,7 @@ for q in [0,Lx-1]: # loop only over quantum numbers 0, pi so all symmetries comm
 
 	basis_fermion = spinless_fermion_basis_general(N_2d, make_basis=False,
 										Nf=N_2d//2,
-										kxblock=(T_x,q),kyblock=(T_y,q),
-										rblock=(P_d,0),
-										pxblock=(P_x,0),pyblock=(P_y,0),
+										**basis_dict,
 									)
 
 	basis_fermion_full = spinless_fermion_basis_general(N_2d, make_basis=False,
@@ -83,9 +69,7 @@ for q in [0,Lx-1]: # loop only over quantum numbers 0, pi so all symmetries comm
 
 	basis_spinful_fermion = spinful_fermion_basis_general(N_2d, make_basis=False,
 										Nf=(N_2d//8,N_2d//8),
-										kxblock=(T_x,q),kyblock=(T_y,q),
-										rblock=(P_d,0),
-										pxblock=(P_x,0),pyblock=(P_y,0),
+										**basis_dict,
 									)
 
 	basis_spinful_fermion_full = spinful_fermion_basis_general(N_2d, make_basis=False,
@@ -109,44 +93,33 @@ for q in [0,Lx-1]: # loop only over quantum numbers 0, pi so all symmetries comm
 		else: # boson, spin
 			static=[['zz',J_p],['+-',J_p],['-+',J_p]]
 
-		print('# of states', q, i, basis_2d_made.Ns, basis_2d_full_made.Ns)
+		print('# of states', i, basis_2d_made.Ns, basis_2d_full_made.Ns)
 
-		H=hamiltonian(static,[],basis=basis_2d_made,dtype=np.float64)
-		H_full=hamiltonian(static,[],basis=basis_2d_full_made,dtype=np.float64)
+		H=hamiltonian(static,[],basis=basis_2d_made,dtype=np.complex128)
 
 		E_GS,V_GS=H.eigsh(k=1,which='SA')
-		#E_GS,V_GS=H.eigh()
-		E_GS_full,V_GS_full=H_full.eigsh(k=1,which='SA')
-		#E_GS_full,V_GS_full=H_full.eigh() #
-
-		#print(np.linalg.norm( (H -H.conj().T).toarray() ))
-		#print(np.linalg.norm( (H_full -H_full.conj().T).toarray() ))
-		#print(E_GS_full[0],E_GS[0])
-		#exit()
-
-		np.testing.assert_allclose(E_GS - E_GS_full,0.0,atol=1E-5,err_msg='failed energies comparison!')
 
 	
-		amps=np.zeros(basis_2d.Ns,dtype=H.dtype)
 		states=basis_2d_made.states.copy()
 		inds=[np.where(basis_2d_full.states==r)[0][0] for r in states]
-		psi_GS=V_GS[:,0]
+		psi_GS=V_GS[:,0].copy()
+		#print(psi_GS[:4]/psi_GS[0])
 		out=basis_2d.get_amp(states,amps=psi_GS,mode='representative')
-		print(psi_GS[0:4]/psi_GS[0])
-		print((V_GS_full[inds,0]/(V_GS_full[inds,0][0]))[:4])
-		np.testing.assert_allclose(psi_GS/psi_GS[0] - V_GS_full[inds,0]/(V_GS_full[inds,0][0]),0.0,atol=1E-5,err_msg='failed representative mode comparison!')
+		print(psi_GS[0:4])
+		psi_tmp=basis_2d_made.get_vec(V_GS[:,0],pcon=True,sparse=False)[inds]
+		print((psi_tmp	)[:4])
+		print(psi_GS[0:4] - psi_tmp[0:4] )
+		np.testing.assert_allclose(psi_GS - psi_tmp,0.0,atol=1E-5,err_msg='failed representative mode comparison!')
+		
 
-		#exit()
-
-		amps_full=np.zeros(basis_2d_full.Ns,dtype=H_full.dtype)
 		states_full=basis_2d_full_made.states.copy()
-		psi_GS_full=V_GS_full[:,0]
+		psi_GS_full=basis_2d_made.get_vec(V_GS[:,0],pcon=True,sparse=False) #V_GS_full[:,0].astype(np.complex128)
 		basis_2d.get_amp(states_full,amps=psi_GS_full,mode='full_basis')
 		ref_states=np.sort( np.unique(states_full) )[::-1]
 		psi_GS_full=psi_GS_full[inds]
-		np.testing.assert_allclose(psi_GS_full/psi_GS_full[0] - V_GS[:,0]/V_GS[0,0],0.0,atol=1E-5,err_msg='failed full_basis mode comparison!')
+		np.testing.assert_allclose(psi_GS_full - V_GS[:,0],0.0,atol=1E-5,err_msg='failed full_basis mode comparison!')
 
 
 
-		print('test {} passed'.format(i))
+		print('test {0}/{1} passed'.format(ii,i))
 