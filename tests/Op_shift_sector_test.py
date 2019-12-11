962ca1e9b4c8c400106bd06ca5d88376b73efb02
weinbe58 <weinbe58@gmail.com>
2019-12-11 08:44:47 -0500

    fixing test

--- tests/Op_shift_sector_test.py
+++ tests/Op_shift_sector_test.py
@@ -1,8 +1,14 @@
+from __future__ import print_function, division
+
+import sys,os
+qspin_path = os.path.join(os.getcwd(),"../")
+sys.path.insert(0,qspin_path)
+
 from quspin.basis import spin_basis_general
 import numpy as np
 
 
-L = 16
+L = 13
 
 
 for k in range(L):
@@ -35,6 +41,39 @@ for k in range(L):
 
 		v_out = b2.Op_shift_sector(b1,op_list,v_in)
 
+		np.testing.assert_allclose(v_out,v_out_proj,rtol=0, atol=1e-13)
+
+
+
+for q1 in [0,1]:
+	for q2 in [0,1]:
+		i = 1
+		print("testing q1={} -> q2={}".format(q1,q2))
+		op_list = [["z",[i],1],["z",[L-i-1],(-1)**q2]]
+
+		p = np.arange(L)[::-1]
+		# z = -(np.arange(L)+1)
+
+
+		b = spin_basis_general(L)
+		b1 = spin_basis_general(L,block=(p,q1))
+		b2 = spin_basis_general(L,block=(p,q1+q2))
+
+		# print(b1)
+		# print(b2)
+
+		P1 = b1.get_proj(np.complex128)
+		P2 = b2.get_proj(np.complex128)
+
+		v_in = np.random.normal(0,1,size=b1.Ns) + 1j*np.random.normal(0,1,size=b1.Ns)
+		v_in /= np.linalg.norm(v_in)
+
+
+		v_in_full = P1.dot(v_in)
+		v_out_full = b.inplace_Op(v_in_full,op_list,np.complex128)
+		v_out_proj = P2.H.dot(v_out_full)
+
+		v_out = b2.Op_shift_sector(b1,op_list,v_in)
 		np.testing.assert_allclose(v_out,v_out_proj,rtol=0, atol=1e-13)
 
 
