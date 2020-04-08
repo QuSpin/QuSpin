2cd019d76c612cf466d8e504b11e0f9f17db6711
mgbukov <mgbukov@gmail.com>
2020-04-08 13:19:51 -0700

    adding test; test fails

---
+++ tests/get_vec_inv_test.py
@@ -0,0 +1,53 @@
+from __future__ import print_function, division
+
+import sys,os
+quspin_path = os.path.join(os.getcwd(),"../")
+sys.path.insert(0,quspin_path)
+
+from quspin.operators import hamiltonian
+from quspin.basis import spin_basis_general
+import numpy as np
+import scipy.sparse as sp
+from numpy.linalg import norm
+from numpy.random import random,seed
+
+seed(0)
+
+def test(basis,pcon=False):
+
+	P = basis.get_proj(np.complex128,pcon=pcon)
+
+	print(P.toarray())
+
+	Ns_full, Ns = P.shape
+
+	v=np.random.normal(size=Ns)
+	v/=np.linalg.norm(v)
+
+	v_full=np.random.normal(size=Ns_full)
+	v_full/=np.linalg.norm(v_full)
+
+	err_msg = "get_vec/get_vec_inv test failed for L={0}".format(basis.__class__)
+	
+	np.testing.assert_allclose(P.dot(v)       , basis.get_vec(v, sparse=False)         ,atol=1e-10,err_msg=err_msg)
+
+	v_p = P.H.dot(v_full)
+	v_gv = basis.get_vec_inv(v_full, sparse=False)
+
+	print(v_p)
+	print(v_gv)
+	exit()
+
+	np.testing.assert_allclose(P.H.dot(v_full), basis.get_vec_inv(v_full, sparse=False),atol=1e-10,err_msg=err_msg)
+
+
+
+L=2
+bases=[spin_basis_general(L,Nup=L//2), ] # spin_basis_general(L,), 
+
+
+for basis in bases:
+
+	test(basis)
+
+
