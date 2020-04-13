a6aa287d2819426d97e800c692abdd1e52dd2b4a
mgbukov <mgbukov@gmail.com>
2020-04-13 13:50:24 -0700

    slow ent_entropy for S=1

--- examples/scripts/example22.py
+++ examples/scripts/example22.py
@@ -15,9 +15,13 @@ from quspin.basis import spin_basis_1d
 from quspin.operators import hamiltonian
 from quspin.tools.evolution import expm_multiply_parallel
 import numpy as np
+import time
+#
+dtype_real = np.float64
+dtype_cmplx = np.result_type(dtype_real,np.complex64)
 #
 ##### define model parameters #####
-L=10 # system size
+L=12 # system size
 T=1.5 # period of switching
 N_steps=100 # of driving cycles
 Jxy=np.sqrt(2.0) # xy interaction
@@ -26,7 +30,7 @@ hz=1.0/np.sqrt(3.0) # z external field
 #
 ##### set up Heisenberg Hamiltonian in an external z-field #####
 # compute basis
-basis = spin_basis_1d(L,S="1",Nup=L//2,kblock=0,pblock=1,) # and positive parity sector
+basis = spin_basis_1d(L,S="1",m=0,kblock=0,pblock=1,) # and positive parity sector
 print('total number of basis states {}'.format(basis.Ns))
 # define operators with OBC using site-coupling lists
 J_zz = [[Jzz_0,i,(i+1)%L] for i in range(L)] # OBC
@@ -38,9 +42,9 @@ static_0 = [["+-",J_xy],["-+",J_xy],]
 static_1 = [["zz",J_zz],["z",h_z],]
 dynamic=[]
 # compute the time-dependent Heisenberg Hamiltonian
-H_ave = 0.5*hamiltonian(static_ave,dynamic,basis=basis,dtype=np.float64)
-H0 = hamiltonian(static_0,dynamic,basis=basis,dtype=np.float64)
-H1 = hamiltonian(static_1,dynamic,basis=basis,dtype=np.float64)
+H_ave = 0.5*hamiltonian(static_ave,dynamic,basis=basis,dtype=dtype_real)
+H0 = hamiltonian(static_0,dynamic,basis=basis,dtype=dtype_real)
+H1 = hamiltonian(static_1,dynamic,basis=basis,dtype=dtype_real)
 #
 ##### various exact diagonalisation routines #####
 # calculate full eigensystem
@@ -52,33 +56,46 @@ psi=psi_i.copy().astype(np.complex128)
 work_array=np.zeros((2*len(psi),), dtype=psi.dtype) # twice as long because complex-valued
 
 # construct unitaries
-expH0 = expm_multiply_parallel(H0.tocsr(),a=-1j*0.5*T)
-expH1 = expm_multiply_parallel(H1.tocsr(),a=-1j*0.5*T) 
+expH0 = expm_multiply_parallel(H0.tocsr(),a=-1j*0.5*T,dtype=dtype_cmplx)
+expH1 = expm_multiply_parallel(H1.tocsr(),a=-1j*0.5*T,dtype=dtype_cmplx) 
 
 # preallocate variables
-E_density=np.zeros(N_steps+1,dtype=np.float64)
-Sent_density=np.zeros(N_steps+1,dtype=np.float64)
+E_density=np.zeros(N_steps+1,dtype=dtype_real)
+Sent_density=np.zeros(N_steps+1,dtype=dtype_real)
 
 
-E_density[0]=H_XXZ.expt_value(psi).real/L
+E_density[0]=H_ave.expt_value(psi).real/L
 Sent_density[0]=basis.ent_entropy(psi,sub_sys_A=range(L//2),density=True)['Sent_A']
 
 
+
+ti=time.time()
 for j in range(N_steps):
 	
 	# apply to state psi and update psi in-place
 	expH0.dot(psi,work_array=work_array,overwrite_v=True) 
 	expH1.dot(psi,work_array=work_array,overwrite_v=True)
 
-	E_density[j+1]=H_XXZ.expt_value(psi).real/L
+	E_density[j+1]=H_ave.expt_value(psi).real/L
 	Sent_density[j+1]=basis.ent_entropy(psi,sub_sys_A=range(L//2),density=True)['Sent_A']
 
 	print("finished evolving {0:d} step".format(j))
+tf=time.time()
 
+print(tf-ti)
+
+
+m=basis.sps**(L//2)
+n=basis.sps**L
+s_p = (np.log(m) - m/(2.0*n) ) / (L//2) 
+
+print(s_p)
+#exit()
 
 import matplotlib.pyplot as plt # plotting library
 times=T*np.arange(N_steps+1)
 plt.plot(times, E_density)
+plt.plot(times, s_p*times/times)
 plt.plot(times, Sent_density)
 plt.show()
 
