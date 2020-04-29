8ba5ee23ba9437ae34a6b186300cc4140fd3b5d4
mgbukov <mgbukov@gmail.com>
2020-04-29 12:14:12 -0700

    fixing typo

--- application/quspin_app.py
+++ application/quspin_app.py
@@ -31,48 +31,56 @@ from scipy.sparse.linalg import eigsh
 L=int(sys.argv[3])
 # time evolution parameters: expect linear slowdown with incerasing N_T
 N_T=int(sys.argv[4])
-
+T=1.0
 
 ######## BASIS CONSTRUCTION ########
 #
 # required time scales exponentially with L
 # linear speedup is expected from OMP
 
-if L>6:
-	print("running time expected to take too long!\n \
-		   If you'd nonetheless still like to run it, change these lines explicitly \
-		   and increase Ns_block_est in the basis construction below appropriately.\n")
-	assert(L<=6)
+# if L>6:
+# 	print("running time expected to take too long!\n \
+# 		   If you'd nonetheless still like to run it, change these lines explicitly \
+# 		   and increase Ns_block_est in the basis construction below appropriately.\n")
+# 	assert(L<=6)
 
-Lx=L
-Ly=L
-N_sites=L*L
+# Lx=L
+# Ly=L
+# N_sites=L*L
 
-sites = np.arange(N_sites,dtype=np.int32) # sites [0,1,2,....]
+# sites = np.arange(N_sites,dtype=np.int32) # sites [0,1,2,....]
 	
-x = sites%Lx # x positions for sites
-y = sites//Lx # y positions for sites
+# x = sites%Lx # x positions for sites
+# y = sites//Lx # y positions for sites
 
-T_x = (x+1)%Lx + Lx*y # translation along x-direction
-T_y = x +Lx*((y+1)%Ly) # translation along y-direction
+# T_x = (x+1)%Lx + Lx*y # translation along x-direction
+# T_y = x +Lx*((y+1)%Ly) # translation along y-direction
 
-P_x = x + Lx*(Ly-y-1) # reflection about x-axis
-P_y = (Lx-x-1) + Lx*y # reflection about y-axis
-P_d = y + Lx*x
+# P_x = x + Lx*(Ly-y-1) # reflection about x-axis
+# P_y = (Lx-x-1) + Lx*y # reflection about y-axis
+# P_d = y + Lx*x
 
-Z   = -(sites+1) # spin inversion
+# Z   = -(sites+1) # spin inversion
 
-###### setting up bases ######
+# ###### setting up bases ######
+# ti=time.time()
+# basis = spin_basis_general(L*L, pauli=False, Ns_block_est=16000000,
+# 								Nup=N_sites//2,
+# 						#		kxblock=(T_x,0),kyblock=(T_y,0),
+# 								pdblock=(P_d,0),
+# 								pxblock=(P_x,0),pyblock=(P_y,0),
+# 								zblock=(Z,0),
+# 								block_order=['zblock','pdblock','pyblock','pxblock'] #,'kyblock','kxblock']
+# 							)
+
+p = np.arange(L)[::-1]
+t = (np.arange(L)+1)%L
+z = -(np.arange(L)+1)
+
 ti=time.time()
-basis = spin_basis_general(L*L, pauli=False, Ns_block_est=16000000,
-								Nup=N_sites//2,
-								kxblock=(T_x,0),kyblock=(T_y,0),
-								pdblock=(P_d,0),
-								pxblock=(P_x,0),pyblock=(P_y,0),
-								zblock=(Z,0),
-								block_order=['zblock','pdblock','pyblock','pxblock','kyblock','kxblock']
-							)
+basis = spin_basis_general(L,S="1/2",m=0,kblock=(t,0),pblock=(p,0),zblock=(z,0))
 tf=time.time()
+
 time_basis=tf-ti
 print("\nbasis with {0:d} states took {1:0.2f} secs.\n".format(basis.Ns, time_basis))
 
@@ -83,13 +91,17 @@ print("\nbasis with {0:d} states took {1:0.2f} secs.\n".format(basis.Ns, time_ba
 # required time scales exponentially with L
 # linear speedup is expected from both OMP and MKL
 
-J_list=[[1.0,i,T_x[i]] for i in range(N_sites)] + [[1.0,i,T_y[i]] for i in range(N_sites)]
-static=[ ["+-",J_list], ["-+",J_list], ["zz",J_list] ]
+
+def drive(t,T):
+	return np.sin(0.5*np.pi*t/T)**2
+
+J_list=[[1.0,i,t[i]] for i in range(L)] 
+static=[ ["+-",J_list], ["-+",J_list],  ]
+dynamic=[["zz",J_list,drive,(T,)],]
 
 ti=time.time()
-H=hamiltonian(static, [], basis=basis, dtype=np.float64)
+H=hamiltonian(static, dynamic, basis=basis, dtype=np.float64)
 H_op=H.aslinearoperator()
-H_qop=quantum_LinearOperator(static,basis=basis,dtype=np.float64)
 tf=time.time()
 time_H=tf-ti
 print("\nHamiltonian construction took {0:0.2f} secs.\n".format(time_H))
@@ -109,33 +121,15 @@ time_eigsh_op=tf-ti
 print("\ncomputing ground state took {0:0.2f} secs.\n".format(time_eigsh_op))
 
 
-ti=time.time()
-H_qop.eigsh(k=1,which='SA')
-tf=time.time()
-time_eigsh_qop=tf-ti
-print("\ncomputing ground state for quantum_LinearOperator took {0:0.2f} secs.\n".format(time_eigsh_qop))
 
 
 # calculate minimum and maximum energy only
 ti=time.time()
-Es, psi_s=H.eigsh(k=2,which="BE",maxiter=1E4,)
+Es, psi_s=H.eigsh(k=1,which="SA",maxiter=1E4,)
 tf=time.time()
 time_eigsh=tf-ti
 print("\ncomputing ground state & most excited state took {0:0.2f} secs.\n".format(time_eigsh))
 
-
-
-
-######## project_from & project_to ########
-#
-# required time scales exponentially with L
-# linear speedup is expected from OMP
-ti=time.time()
-psi_full = basis.project_from(psi_s[:,0],sparse=False) 
-basis.project_to(psi_full)
-tf=time.time()
-time_proj=tf-ti
-print("\nbasis.project_* took {0:0.2f} secs.\n".format(time_proj))
 
 
 
@@ -146,14 +140,22 @@ print("\nbasis.project_* took {0:0.2f} secs.\n".format(time_proj))
 
 
 # calculate the eigenstate closest to energy E_star
-psi_i=1.0/np.sqrt(2)*(psi_s[:,0] + psi_s[:,1])
+psi_i=psi_s[:,0]
+
+ti=time.time()
+times=np.linspace(0.0,T,num=N_T)
+psi_t=H.evolve(psi_i,times[0],times,iterate=True)
+for psi in psi_t:
+	pass
+tf=time.time()
+time_SE=tf-ti
+print("\ncomputing SE took {0:0.2f} secs.\n".format(time_SE))
 
 
-dt=0.1
+dt=T/N_T
 expH = expm_multiply_parallel(H.tocsr(),a=-1j*dt,dtype=np.complex128) 
 #
 # auxiliary array for memory efficiency
-psi=psi_i.copy().astype(np.complex128)
 work_array=np.zeros((2*len(psi),), dtype=psi.dtype) # twice as long because complex-valued
 #
 # loop ober the time steps
@@ -162,7 +164,6 @@ for j in range(N_T):
 	#
 	# apply to state psi and update psi in-place
 	expH.dot(psi,work_array=work_array,overwrite_v=True)
-	
 tf=time.time()
 time_expm=tf-ti
 print("\ntime evolution took {0:0.2f} secs.\n".format(time_expm))
@@ -171,7 +172,7 @@ print("\ntime evolution took {0:0.2f} secs.\n".format(time_expm))
 
 
 
-time_tot=time_basis + time_H + time_eigsh_op + time_eigsh_qop + time_eigh + time_expm + time_proj
+time_tot=time_basis + time_H + time_eigsh_op + time_SE + time_expm 
 print("\n\ntotal run time: {0:0.2f} secs.\n".format(time_tot))
 
 
